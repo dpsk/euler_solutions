19230248ae391eef0b0456a26699f03c9d0ec0db
Nikalyukin Mikhail <idups1k@gmail.com>
2012-03-28 17:14:58 +0300

    brutforce solution for problem #5

---
+++ 5.rb
@@ -0,0 +1,8 @@
+a = 2520
+while true
+  if a%20==0 and a%19==0 and a%18==0 and a%16==0 and a%13==0 and a%17==0 and a%15==0 and a%14==0 and a%12==0 and a%11==0
+    puts a
+    break;
+  end
+  a+=1
+end
